411b8feddc85239c2a1255e51cc2b6537e5dfda9
Wankupi <2893353848@qq.com>
2023-04-13 11:26:37 +0800

    feat(bpt): add interface of class bpt

--- src/bpt.h
+++ src/bpt.h
@@ -3,15 +3,20 @@
 #define B_TREE_BPT_H
 #include "bpt_exception.h"
 #include <memory>
+#include <vector>
 /**
  * @brief a multimap
  * @tparam Key type of key
  * @tparam Val type of value
  * @tparam Alloc type of allocator
+ * @attention one Key may have various values, but {key, value} is considered distinct.
  */
 template<typename Key, typename Val, template<typename Type> class Alloc = std::allocator>
 class bpt {
 public:
+	bool insert(Key const &index, Val const &val);
+	bool erase(Key const &index, Val const &val);
+	std::vector<Val> find(Key const &index);
 private:
 };
 
