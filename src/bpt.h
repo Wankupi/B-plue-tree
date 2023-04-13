aa84271e9430805783be3da36d45e1818abc06b4
Wankupi <2893353848@qq.com>
2023-04-13 11:19:10 +0800

    create(bpt): bpt.h init

---
+++ src/bpt.h
@@ -0,0 +1,18 @@
+#pragma
+#ifndef B_TREE_BPT_H
+#define B_TREE_BPT_H
+#include "bpt_exception.h"
+#include <memory>
+/**
+ * @brief a multimap
+ * @tparam Key type of key
+ * @tparam Val type of value
+ * @tparam Alloc type of allocator
+ */
+template<typename Key, typename Val, template<typename Type> class Alloc = std::allocator>
+class bpt {
+public:
+private:
+};
+
+#endif//B_TREE_BPT_H
