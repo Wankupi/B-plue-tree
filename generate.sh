9cf410838a3aa713d3832259e551115e3c31a99b
Wankupi <2893353848@qq.com>
2023-04-26 20:18:35 +0800

    test(main): updated generate.sh

--- generate.sh
+++ generate.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-src_files=("src/bpt_exception.h" "src/file/DataBase.h" "src/cache/memory.h" "src/cache/file.h" "src/bpt.h" "test/main/main.cpp")
+src_files=("src/bpt_exception.h"
+	"src/stlite/algorithm.h"
+	"src/stlite/hash_table.h"
+	"src/stlite/list.h"
+	"src/stlite/vector.h"
+	"src/file/DataBase.h"
+	"src/cache/memory.h"
+	"src/cache/file.h"
+	"src/bpt.h"
+	"test/main/main.cpp")
 
 echo "">tmp.cpp
 for file in ${src_files[@]}
